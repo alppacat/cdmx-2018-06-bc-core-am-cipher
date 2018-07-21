bd8cd1e914b0ca0151ffddcdc8c75f915f79b2ad
alppacat <lorena.pratts@gmail.com>
2018-07-21 11:11:47 -0500

    Arregla estilos

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
 
-button1.addEventListener("click", event => {
-  let userText = document.getElementById("userText").value;
+button1.addEventListener('click', event => {
+  let userText = document.getElementById('userText').value;
   let results = cipher.encode(offset, userText);
-  document.getElementById("messageCipher").innerHTML = results;
+  document.getElementById('messageCipher').innerHTML = results;
 });
-button2.addEventListener("click", event => {
-  let userOut = document.getElementById("userOut").value;
+button2.addEventListener('click', event => {
+  let userOut = document.getElementById('userOut').value;
   let results2 = cipher.decode(offset, userOut);
-  document.getElementById("messageDesCipher").innerHTML = results2;
+  document.getElementById('messageDesCipher').innerHTML = results2;
 });
 
-container.style.display = "none";
-hh1.style.display = "none";
+container.style.display = 'none';
+hh1.style.display = 'none';
 
 function buttonWelcome() {
-  loader.style.display = "none";
-  animations.style.display ="none";
-  container.style.display = "flex";
-  hh1.style.display = "block";
+  loader.style.display = 'none';
+  animations.style.display = 'none';
+  container.style.display = 'flex';
+  hh1.style.display = 'block';
   // loader.style.display= "none";
 
   // titlepage.style.display = "block";
   // ingreso.style.display= "block";
-
 }
